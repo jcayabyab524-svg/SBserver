77cc170c321fd11df25f354d1d6233879376222d
jcayabyab524-svg <jcayabyab524@gmail.com>
2026-01-27 15:02:05 +0800

    Update signupserver.js

--- signupserver.js
+++ signupserver.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 10000;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET'
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const app = express();
